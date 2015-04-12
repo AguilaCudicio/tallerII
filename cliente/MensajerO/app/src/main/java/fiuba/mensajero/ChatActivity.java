b8da106a38674a005a5e28fa2cbe30b72e08f1be
BarAgCud <aguila.cudicio@gmail.com>
2015-04-12 20:37:10 -0300

    Primera version, se ve mal

--- cliente/MensajerO/app/src/main/java/fiuba/mensajero/ChatActivity.java
+++ cliente/MensajerO/app/src/main/java/fiuba/mensajero/ChatActivity.java
@@ -1,11 +1,21 @@
 package fiuba.mensajero;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends ActionBarActivity {
     @Override
@@ -13,18 +23,13 @@ public class ChatActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-
         // Get the message from the intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(ListViewFriendsActivity.EXTRA_MESSAGE);
+        String[] lista = new String[] {message};
 
-        // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
-
-        // Set the text view as the activity layout
-        setContentView(textView);
+        MyFragment fragment = new MyFragment();
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
 
     }
 
@@ -40,4 +45,75 @@ public class ChatActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+}
+
+
+class MyFragment extends ListFragment {
+    private List<ListViewItem> mItems;
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // initialize the items list
+       mItems = new ArrayList<ListViewItem>();
+        Resources resources = getResources();
+
+        //TODO: Mensajes de prueba, cambiarlos.
+
+
+        mItems.add(new ListViewItem("Juanito","hola"));
+        mItems.add(new ListViewItem("Juanito","chau"));
+        setListAdapter(new ListViewDemoAdapter(getActivity(), mItems));
+    }
+
+   //public void onListItemClick(ListView listView, View view, int position, long id) {
+    //    ...
+    //}
+}
+class ListViewItem {
+    public final String title; // the text for the ListView item title
+    public final String description; // the text for the ListView item description
+    public ListViewItem(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
+
+
+class ListViewDemoAdapter extends ArrayAdapter<ListViewItem> {
+
+    public ListViewDemoAdapter(Context context, List<ListViewItem> items) {
+        super(context, R.layout.listview_item, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if(convertView == null) {
+            // inflate the GridView item layout
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.listview_item, parent, false);
+            // initialize the view holder
+            viewHolder = new ViewHolder();
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvDescription = (TextView) convertView.findViewById(R.id.tvDescription);
+            convertView.setTag(viewHolder);
+        } else {
+            // recycle the already inflated view
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        // update the item view
+        ListViewItem item = getItem(position);
+        viewHolder.tvTitle.setText(item.title);
+        viewHolder.tvDescription.setText(item.description);
+        return convertView;
+    }
+    /**
+     * The view holder design pattern prevents using findViewById()
+     * repeatedly in the getView() method of the adapter.
+     *
+     */
+    private static class ViewHolder {
+        TextView tvTitle;
+        TextView tvDescription;
+    }
 }
