a0b888c11823a22795618490bdac3450441da255
BarAgCud <aguila.cudicio@gmail.com>
2015-04-12 14:45:45 -0300

    Lista de personas con icono para indicar si estan conectadas o no

--- cliente/MensajerO/app/src/main/java/fiuba/mensajero/ListViewFriendsActivity.java
+++ cliente/MensajerO/app/src/main/java/fiuba/mensajero/ListViewFriendsActivity.java
@@ -1,19 +1,39 @@
 package fiuba.mensajero;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import static android.app.AlertDialog.*;
 
 public class ListViewFriendsActivity extends ListActivity {
+    static String EXTRA_MESSAGE ="";
+    String[] contactos;
+
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         /* TODO: Aca deberian ir las personas conectadas*/
-        String[] contactos = new String[] { "conectadoJuan", "conectadoFernando", "conectadoMaria", "conectadoRoberto",
+        contactos = new String[] { "conectadoJuan", "conectadoFernando", "conectadoMaria", "conectadoRoberto",
                 "conectadoAna", "Irene", "Matilda", "Alma",
                 "Francisco", "Adrian", "Elena", "Jesica", "Roberto",
                 "Matias", "Soledad", "Victoria", "Nadia", "Elsa" };
         AdaptFriends adapt = new AdaptFriends (this, contactos);
         setListAdapter(adapt);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        AlertDialog alertDialog = new Builder(this).create();
+
+        Intent intent = new Intent(this, ChatActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "Conversacion con "+contactos[position]);
+        startActivity(intent);
+
+        super.onListItemClick(l, v, position, id);
     }
 
 }
