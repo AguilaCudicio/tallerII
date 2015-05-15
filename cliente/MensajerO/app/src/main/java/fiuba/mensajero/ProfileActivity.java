501f18e3ab6e710f99a7b347b79bbc801ae1ca64
BarAgCud <aguila.cudicio@gmail.com>
2015-05-14 22:58:39 -0300

    Continuando perfil. Corregido el bug en la vista de pantalla completa del avatar. Faltan las opciones para modificar avatar

--- cliente/MensajerO/app/src/main/java/fiuba/mensajero/ProfileActivity.java
+++ cliente/MensajerO/app/src/main/java/fiuba/mensajero/ProfileActivity.java
@@ -1,18 +1,15 @@
 package fiuba.mensajero;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 
 public class ProfileActivity extends ActionBarActivity {
@@ -41,14 +38,28 @@ public class ProfileActivity extends ActionBarActivity {
             @Override
             public void onClick(View arg0) {
 
-                Dialog settingsDialog = new Dialog(ProfileActivity.this);
-                settingsDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-                settingsDialog.setContentView(getLayoutInflater().inflate(R.layout.profilepicture_layout, null));
-                settingsDialog.show();
+                final Dialog profileDialog = new Dialog(ProfileActivity.this);
+                profileDialog.setTitle("Foto de perfil");
+
+                profileDialog.setContentView(getLayoutInflater().inflate(R.layout.profilepicture_layout, null));
+
+                Button button = (Button) profileDialog.findViewById(R.id.ButtonProfile);
+                button.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        profileDialog.cancel();
+                    }
+                });
+                profileDialog.show();
             }
         });
 
     }
+
+   public void changeActivityEditProfile() {
+       Intent flist = new Intent(this, EditProfileActivity.class);
+       startActivity(flist);
+   }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
