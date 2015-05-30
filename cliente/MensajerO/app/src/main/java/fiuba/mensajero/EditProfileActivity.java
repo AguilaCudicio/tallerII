84aaca0d52e00a2d8861c8dc2a71bc12c389e96b
BarAgCud <aguila.cudicio@gmail.com>
2015-05-30 07:03:59 -0300

    Agregando las primeras funcionalidades del avatar: Intent que abre la galería

--- cliente/MensajerO/app/src/main/java/fiuba/mensajero/EditProfileActivity.java
+++ cliente/MensajerO/app/src/main/java/fiuba/mensajero/EditProfileActivity.java
@@ -1,17 +1,75 @@
 package fiuba.mensajero;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class EditProfileActivity extends ActionBarActivity {
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+    }
+
+
+    //* handler para el boton de cargar imagen.
+    // Crea un intent de galería
+    public void uploadImage(View view) {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.uploadimageview);
+                // Poner en ImageView
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(this, "Tiene que seleccionar una imagen",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Se ha producido un error!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void saveChanges(View view) {
+        // TODO Aca deberia llamar a ProfileData y guardar el nuevo avatar, si fue cargado...
+        finish();
     }
 
 
